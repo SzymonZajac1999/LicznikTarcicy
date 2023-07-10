d85eb6faf4ece1e7bd3916c33d48b8136022b70f
Marcin Kwiatkowski <marcinkwiatkowski04@gmail.com>
2023-07-10 22:39:45 +0200

    drobne poprawki

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace LicznikTArcicy
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
@@ -32,8 +16,10 @@ namespace LicznikTArcicy
             double szerokoscDeski = double.Parse(szerokoscDeskiTextbox.Text);
             double wysokoscDeski = double.Parse(wysokoscDeskiTextbox.Text);
             double iloscDesek = double.Parse(iloscdesekTextbox.Text);
+
             double polePowierzchni = dlugoscDeski * szerokoscDeski * iloscDesek;
             double objetosc = dlugoscDeski * szerokoscDeski * wysokoscDeski * iloscDesek;
+
             PolePowierzchni.Content = "Pole powierzchni to: " + polePowierzchni.ToString() + " m2";
             Objetosc.Content = "Suma objętości to: " +  objetosc.ToString() + " m3";
         }
